6f303ec93ee4b342c84356835a639607f504d4f5
Jaxson Otis <jotisbsb@outlook.com>
2022-05-27 23:28:08 -0700

    asdklf

--- src/components/Account.js
+++ src/components/Account.js
@@ -4,7 +4,11 @@ const Account = () => {
     const navigate = useNavigate();
 
     const username = sessionStorage.getItem("user")
-    
+    console.log(username);
+
+    const userId = document.getElementById("usernameP");
+    userId.textContent = username;
+
     const handleLogout = () => {
         sessionStorage.clear();
         navigate('/login');
@@ -17,7 +21,7 @@ const Account = () => {
   return (
     <section id="accounts">
         <h1 id="accountH1">Account</h1>
-        <p value={username}></p>
+        <p id='usernameP'></p>
         <p>Please note that delete functionality is currently unavailable.</p>
 
         <button id="logoutB" onClick={handleLogout}>Logout</button>
