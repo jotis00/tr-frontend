ead9e2ec89e70a97e5d22aa862014a526d3cfa20
Jaxson Otis <jotisbsb@outlook.com>
2022-05-28 21:24:24 -0700

    title and logo to browser

--- src/components/StateCapitalTest.js
+++ src/components/StateCapitalTest.js
@@ -1,6 +1,25 @@
+import { useState } from "react"
+import axios from "../api/axios";
+import ScMap from "../assets/capitals.webp";
+import ScList from "..//assets/state and capital list.gif";
+const TEST_URL = "/api/test";
+const nameOfTest = "stateCapital";
+
+
 const StateCapitalTest = () => {
+  const [gameStart, setGameStart] = useState(false);
+
+  const start = () => {
+
+  }
+
   return (
-    <section>StateCapitalTest</section>
+    <div className={gameStart ? "sct": "sctM"}>
+      <h1>State Capital Test</h1>
+      <button style={{display: 'flex', alignItems: "center"}}disabled={gameStart ? true: false} onClick={start}>Start</button>
+      <img id="sctm" src={ScMap} alt="State Capital Map" className={ gameStart ? "offscreen": "notHidden"}></img>
+      
+      </div>
   )
 }
 
--- src/components/WordNumberTest.js
+++ src/components/WordNumberTest.js
@@ -98,7 +98,7 @@ const WordNumberTest = () => {
     
   return (
     <section>
-        <h1>Word/Number Test</h1>
+        <h1>Word Number Test</h1>
         <button disabled={gameStart ? true: false} onClick={start}>Start</button>
         <form onSubmit={handleSubmit}>
             <label 
